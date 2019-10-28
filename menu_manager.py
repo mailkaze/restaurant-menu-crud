06534c1e3bce57b0d8ac0ffe63387088da96cc3c
Kaze <mailkaze@gmail.com>
2019-10-28 15:28:00 -0400

    antes de hacer más dinamico el html

---
+++ menu_manager.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, render_template, redirect
+from flask_sqlalchemy import SQLAlchemy
+from config import DATABASE_URI
+from strings import html_producto
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Producto(db.Model):
+    __tablename__ = 'productos'
+    id_producto = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String)
+    descripcion = db.Column(db.String)
+    precio = db.Column(db.Float)
+    stock = db.Column(db.Integer)
+
+    def __repr__(self):
+        return "<Producto(nombre='{}', descripción='{}', precio={}, stock={})>".format(
+                self.nombre, self.descripcion, self.precio, self.stock
+                )
+
+@app.route('/', methods=['GET', 'POST'])
+def menu():
+    if request.method == 'POST':
+        producto = Producto(
+            nombre = request.form.get('nombre'),
+            descripcion = request.form.get('descripcion'),
+            precio = request.form.get('precio'),
+            stock = request.form.get('stock')
+        )
+        db.session.add(producto)
+        db.session.commit()
+
+    productos = Producto.query.all()
+    tarjetas = ''
+    for p in productos:
+        tarjeta = html_producto.format(
+            p.nombre,
+            p.descripcion,
+            p.precio,
+            p.stock
+        )
+        tarjetas += tarjeta
+    #aquí vamos a mandar variables para cargar la tarjeta de edición de producto,
+    #como el titulo de la tarjeta, los valores de los campos si hay info y si el
+    #botón cancelar debe estar oculto o no:
+    return render_template('menu.html', tarjetas=tarjetas)
+
+
+if __name__ == '__main__':
+    db.create_all()
+    db.session.commit()
+    app.run(debug=True)
+    
+
+
---
+++ strings.py
@@ -0,0 +1,57 @@
+html_producto = '''
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">{}</h5>
+    <p class="card-text">{}</p>
+    <p class="card-text">Precio: Bs. {}</p>
+    <p class="card-text">Quedan {} unidades</p>
+    <button type="button" class="btn btn-success">Editar</button>
+    <button type="button" class="btn btn-danger">Borrar</button>
+  </div>
+</div>
+'''
+
+html_producto_input = '''
+<div class="card">
+  <div class="card-header">
+    {}
+  </div>
+  <div class="card-body">
+    <form method="POST" action="/">
+                <div class="input-group flex-nowrap">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="addon-wrapping">Nombre</span>
+                    </div>
+                    <input type="text" class="form-control" value="{}" aria-label="nombre"
+                        aria-describedby="addon-wrapping" name="nombre">
+                </div>
+                <br>
+                <div class="input-group">
+                        <div class="input-group-prepend">
+                          <span class="input-group-text">Descripción</span>
+                        </div>
+                        <textarea class="form-control" value="{}" aria-label="With textarea" name="descripcion"></textarea>
+                      </div>
+                <br>
+                <div class="input-group flex-nowrap">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="addon-wrapping">Precio</span>
+                    </div>
+                    <input type="text" class="form-control" value="{}" aria-label="precio"
+                        aria-describedby="addon-wrapping" name="precio">
+                </div>
+                <br>
+                <div class="input-group flex-nowrap">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="addon-wrapping">Stock</span>
+                    </div>
+                    <input type="text" class="form-control" value="{}" aria-label="stock"
+                        aria-describedby="addon-wrapping" name="stock">
+                </div>
+                <br>
+                <button type="{}" class="btn btn-secondary">Cancelar</button>
+                <input type="submit" class="btn btn-primary" value="Guardar">
+            </form>
+  </div>
+</div>
+'''
